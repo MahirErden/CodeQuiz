f930965ae8a203429efa388ac80b26a93ca81bc2
Mahir Erden <mahirerden@gmail.com>
2019-11-13 09:56:19 -0500

    questions.js added

---
+++ assets/js/questions.js
@@ -0,0 +1,54 @@
+var questions = [
+     {
+       title: "Inside which HTML element do we put the JavaScript?",
+       choices: ["<script>", "<js>", "<scripting>", "<javascript>"],
+       answer: "<script>"
+     },
+     {
+       title: "How to write an IF statement in JavaScript?",
+       choices: ["if i = 5", "if (i === 5)", "if i == 5 then", "if i = 5 then"],
+       answer: "if (i === 5)"
+     },
+     {
+          title: "How does a FOR loop start?",
+          choices: ["for i = 1 to 5", "for(i = 0; i<=5; i++)", "for(i<=5; i++)", "for(i=0; i<=5)"],
+          answer: "for(i = 0; i<=5; i++)"
+     },
+     {
+          title: "How can you add a comment in a JavaScript?",
+          choices: ["<!--This is a comment-->", "'This is a comment", "//This is a comment", "<<<This is a comment>>>"],
+          answer: "//This is a comment"
+     },
+     {
+          title: "What is the correct way to write a JavaScript array?",
+          choices: ["var colors = \[\"red\"\, \"blue\"\, \"green\"\]", "var colors = 1 = (\"red\"),2=(\"blue\"),3=(\"green\")", "var colors = \"red\"\, \"blue\"\, \"green\"", "var colors = (1:\"red\", 2:\"blue\", 3:\"green\")"],
+          answer: "var colors = \[\"red\"\, \"blue\"\, \"green\"\]"
+     },
+     {
+          title: "How do you round the number 7.25, to the nearest integer?",
+          choices: ["rnd(7.25)", "Math.round(7.25)", "round(7.25)", "Math.rnd(7.25)"],
+          answer: "Math.round(7.25)"
+     },
+     {
+          title: "How do you find the number with the highest value of x and y?",
+          choices: ["Math.ceil(x,y)", "ceil(x,y)", "Math.max(x,y)", "top(x,y)"],
+          answer: "Math.max(x,y)"
+     },
+     {
+          title: "Which event occurs when the user clicks on an HTML element?",
+          choices: ["onchange", "onmouseover", "onmouseclick", "onclick"],
+          answer: "onclick"
+     },
+     {
+          title: "How do you declare a JavaScript variable?",
+          choices: ["var carName;", "v carName;", "variable carName;", "var = carName;"],
+          answer: "var carName;"
+     },
+     {
+          title: "What will the following code return: Boolean(10 > 9)",
+          choices: ["true", "false", "NaN", "undefined"],
+          answer: "true"
+     },
+   ];
+
+   
