80e1d2ddd72f08771d159708a38aba29f0f6ff32
Mahir Erden <mahirerden@gmail.com>
2019-11-18 10:44:34 -0500

    highscores sorted

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@
 var startButton = document.getElementById("startButton");
 var startContainer = document.getElementById("startContainer");
 var questionContainer = document.getElementById("questionContainer");
+var finalscoreContainer = document.getElementById("finalscoreContainer");
+var highscoreContainer = document.getElementById("highscoreContainer");
+var viewHighscores = document.getElementById("viewHighscores");
 var title = document.getElementById("title");
 const choiceButton0 = document.getElementById("choiceButton0");
 const choiceButton1= document.getElementById("choiceButton1");
@@ -10,15 +13,24 @@ const choiceButton3 = document.getElementById("choiceButton3");
 var wrongAudio = document.getElementById("wrongAudio");
 var correctAudio = document.getElementById("correctAudio");
 var answerResult = document.getElementById("answerResult");
+var finalscore = document.getElementById("finalscore");
+var highscoresUl = document.querySelector("#highscoresUl");
+var initial = document.getElementById("initial");
+var initialSubmit = document.getElementById("initialSubmit");
+var goBackButton = document.getElementById("goBackButton");
+var clearHighscoresButton = document.getElementById("clearHighscoresButton");
+let highscoresArray = localStorage.getItem('hscore') ? JSON.parse(localStorage.getItem('hscore')) : [];
 var currentQuestion = 0;
 var score = 0;
 
+localStorage.setItem('hscore', JSON.stringify(highscoresArray));
+var data = JSON.parse(localStorage.getItem('hscore'));
 
 startButton.addEventListener("click", startQuiz);
 
-
 function startQuiz(){
     startContainer.style.display = "none";
+    viewHighscores.disabled = true;
     renderQuestion();
     questionContainer.style.display = "block";
     startInterval();
@@ -97,7 +109,7 @@ function checkAnswer(answerId){
 
 //Timer section
 var t;
-var countdown = 49;
+var countdown = 20;
 
 function afterTimeOut() {
 
@@ -115,15 +127,72 @@ function afterTimeOut() {
     choiceButton3.disabled = false;
     answerResult.style.display = "none";
 
-    if(currentQuestion < questions.length){
+    if((currentQuestion + 1) < questions.length){
         currentQuestion++;
-        console.log(currentQuestion);
         renderQuestion();
     }else{
-        // end the quiz and show the score
-        //scoreRender();
-        //
+        stopInterval();
+        scoreRender();
     }
+}
+
+function scoreRender(){
+    questionContainer.style.display = "none";
+    finalscoreContainer.style.display = "block";
+    finalscore.textContent = score; 
+}
+
+function saveToLocaleStorage(){
+    var highscoresObj = {
+        initials: initial.value.trim(),
+        score: score
+    };
+    highscoresArray.push(highscoresObj);
+    localStorage.setItem('hscore', JSON.stringify(highscoresArray));
+    renderHighscores();
+    
+}
+
+function displayHighscores(){
+    startContainer.style.display = "none";
+    finalscoreContainer.style.display = "none";
+    highscoreContainer.style.display = "block";
+}
+
+function renderHighscores(){
+    displayHighscores()
+    data = JSON.parse(localStorage.getItem('hscore'));
+    data.sort(dynamicSort("-score"));
+    var j = 1;
+    data.forEach(function(item){
+         liMaker(item, j);
+         j++;
+    });
+}
+
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        /* next line works with strings and numbers, 
+         * and you may want to customize it to your needs
+         */
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+}
+
+
+function goBack(){
+    window.location.reload();
+    viewHighscores.disabled = false;
+}
+
+function clearHighscores(){
+    localStorage.clear();
 }
 
 function count() {
@@ -135,7 +204,8 @@ function count() {
     if (confirm("Sorry, You have not completed the quiz on time!\nWould you like to try again")) {
         window.location.reload();
     } else {
-        window.close();
+        exit();
+        //window.close();
     }
   }
 }
@@ -148,14 +218,13 @@ function stopInterval() {
   clearInterval(counter);
 }
 
-//Local Storage section
-function SortLocalStorage(){
-    if(localStorage.length > 0){
-       var localStorageArray = new Array();
-       for (i=0;i<localStorage.length;i++){
-           localStorageArray[i] = localStorage.key(i)+localStorage.getItem(localStorage.key(i));
-       }
-    }
-    var sortedArray = localStorageArray.sort();
-    return sortedArray;
+ function sortHighscoresArray(){
+     highscoresArray = highscoresArray.sort();
  }
+
+ function liMaker(text, index){
+    console.log(index);
+    var li = document.createElement('li');
+    li.textContent = index + " - " + text.initials + " " + text.score;
+    highscoresUl.appendChild(li);
+  }
